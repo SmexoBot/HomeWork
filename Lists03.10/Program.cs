5879d0894a5e92c2cc902372fd651153d36dc1ac
Larushkin Evgeniy <larushkinevgeniy.ru@gmail.com>
2023-11-03 19:01:47 +0500

    Rpn first attempt

--- Lists03.10/Program.cs
+++ Lists03.10/Program.cs
@@ -6,6 +6,86 @@ namespace Lists
 {
     public static class Program
     {
+        public static List<object> ToRpn(string inp)
+        {
+            inp = inp + ' ';
+            inp = inp.Replace('.', ',');
+            int len = inp.Length - 1;
+            List<object> num = new List<object>();
+            Stack<char> op = new Stack<char>();
+            op.Push(' ');
+
+            for (int i = 0; i < len; i++)
+            {
+                if (inp[i] == ' ')
+                {
+                    continue;
+                }
+                else if (Char.IsDigit(inp[i]))
+                {
+                    int index = 1;
+                    while (true)
+                    {
+                        if (Char.IsDigit(inp[i + index]) || inp[i + index] == ',')
+                        {
+                            index++;
+                        }
+                        else
+                        {
+                            num.Add(Convert.ToDouble(inp.Substring(i, index)));
+                            i = i + index - 1;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (op.Peek() == ' ')
+                    {
+                        op.Push(inp[i]);
+                    }
+                    else if (op.Peek() == '(')
+                    {
+                        op.Push(inp[i]);
+                    }
+                    else if(inp[i] == '(')
+                    {
+                        op.Push(inp[i]);
+                    }
+                    else if (inp[i] == ')')
+                    {
+                        while (op.Peek() != '(')
+                        {
+                            num.Add(op.Pop());
+                        }
+                        op.Pop();
+                    }
+                    else if ((op.Peek() == '+' || op.Peek() == '-') && (inp[i] == '+' || inp[i] == '-'))
+                    {
+                        num.Add(op.Pop());
+                        op.Push(inp[i]);
+                    }
+                    /*else if ((op.Peek() == '+' || op.Peek() == '-') && (inp[i] == '*' || inp[i] == '/'))
+                    {
+                        op.Push(inp[i]);
+                    }
+                    */
+                    else if (op.Peek() == '*' || op.Peek() == '/')
+                    {
+                        while (op.Peek() != '+' || op.Peek() != '-')
+                        {
+                            num.Add(op.Pop());
+                        }
+                    }
+                    else
+                    {
+                        op.Push(inp[i]);
+                    }
+                }
+            }
+            return num;
+        }
+
         public static List<double> Number(string inp)
         {
             List<double> num = new List<double>();
@@ -86,6 +166,14 @@ namespace Lists
         }
         public static void Main()
         {
+            string inp = Console.ReadLine();
+            List<object> expression = ToRpn(inp);
+            foreach (object o in expression)
+            {
+                Console.Write(o);
+                Console.Write(' ');
+            }
+            /*
             string input = Console.ReadLine();
             List<double> numbers = Number(input);
             List<(char,int)> operations = Operation(input);
@@ -125,6 +213,8 @@ namespace Lists
                 
             }
             Console.WriteLine(numbers[0]);
+            */
+
         }
     }
 }
