c514644ac30bb37ad9a3f5ff423863faf9263c0e
Larushkin Evgeniy <larushkinevgeniy.ru@gmail.com>
2023-11-04 13:32:26 +0500

    complit rpn

--- Lists03.10/Program.cs
+++ Lists03.10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
@@ -44,13 +45,34 @@ namespace Lists
                     {
                         op.Push(inp[i]);
                     }
-                    else if (op.Peek() == '(')
+
+                    else if (inp[i] == '(')
                     {
                         op.Push(inp[i]);
                     }
-                    else if(inp[i] == '(')
+
+                    else if ((op.Peek() == '+' || op.Peek() == '-') && (inp[i] == '+' || inp[i] == '-'))
                     {
+                        num.Add(op.Pop());
                         op.Push(inp[i]);
+                    }
+                   
+                    else if ((op.Peek() == '*' || op.Peek() == '/'))
+                    {
+                        if ((inp[i] != '*' && inp[i] != '/'))
+                        {
+                            while (op.Peek() != '+' && op.Peek() != '-' && op.Peek() != ' ')
+                            {
+                                num.Add(op.Pop());
+                            }
+                            num.Add(op.Pop());
+                            op.Push(inp[i]);
+                        }
+                        else if (inp[i] == '*' || inp[i] == '/')
+                        {
+                            num.Add(op.Pop());
+                            op.Push(inp[i]);
+                        }
                     }
                     else if (inp[i] == ')')
                     {
@@ -60,98 +82,29 @@ namespace Lists
                         }
                         op.Pop();
                     }
-                    else if ((op.Peek() == '+' || op.Peek() == '-') && (inp[i] == '+' || inp[i] == '-'))
-                    {
-                        num.Add(op.Pop());
-                        op.Push(inp[i]);
-                    }
-                    /*else if ((op.Peek() == '+' || op.Peek() == '-') && (inp[i] == '*' || inp[i] == '/'))
-                    {
-                        op.Push(inp[i]);
-                    }
-                    */
-                    else if (op.Peek() == '*' || op.Peek() == '/')
-                    {
-                        while (op.Peek() != '+' || op.Peek() != '-')
-                        {
-                            num.Add(op.Pop());
-                        }
-                    }
+
                     else
                     {
                         op.Push(inp[i]);
                     }
-                }
+                }   
+            }
+            while (op.Count != 1)
+            {
+                num.Add(op.Pop());
             }
             return num;
         }
-
-        public static List<double> Number(string inp)
+        public static void PrintList(List<object> list)
         {
-            List<double> num = new List<double>();
-            inp += ' ';
-            inp = inp.Replace('.', ',');
-            int len = inp.Length - 1;
-            for (int i = 0; i < len; i++)
+            foreach (object o in list)
             {
-                if (Char.IsDigit(inp[i]))
-                {
-                    int index = 1;
-                    while (true)
-                    {
-                        if (Char.IsDigit(inp[i + index]) || inp[i + index] == ',')
-                        {
-                            index++;
-                        }
-                        else
-                        {
-                            num.Add(Convert.ToDouble(inp.Substring(i, index)));
-                            i = i + index - 1;
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    continue;
-                }
-
+                Console.Write(o);
+                Console.Write(' ');
             }
-            return num;
+            Console.WriteLine();
         }
 
-        public static List<(char,int)> Operation(string inp)
-        {
-            List<(char,int)> op = new List<(char,int)>();
-            inp += ' ';
-            int len = inp.Length - 1;
-            int register = 0;
-            
-            for (int i = 0; i < len; i++)
-            {
-                if ((Char.IsDigit(inp[i]) == false) && (inp[i] != '.') && (inp[i] != ' '))
-                {
-                    
-                    if (inp[i] == '(')
-                    {
-                        register += 2;
-                    }
-                    if (inp[i] == '*' || inp[i] == '/')
-                    {
-                        op.Add((inp[i], register + 1));
-                    }
-                    if (inp[i] == '+' || inp[i] == '-')
-                    {
-                        op.Add((inp[i], register));
-                    }
-                    if (inp[i] == ')')
-                    {
-                        register -= 2;
-                    }
-                }
-            }
-            return op;
-        }
         public static double Calculate(char op, double num1, double num2)
         {
             switch (op)
@@ -164,57 +117,28 @@ namespace Lists
             Console.WriteLine("ERROR");
             return 0;
         }
+
         public static void Main()
         {
             string inp = Console.ReadLine();
             List<object> expression = ToRpn(inp);
-            foreach (object o in expression)
+            PrintList(expression);
+            char check = ' ';
+            
+            for (int i = 0; i < expression.Count; i++)
             {
-                Console.Write(o);
-                Console.Write(' ');
-            }
-            /*
-            string input = Console.ReadLine();
-            List<double> numbers = Number(input);
-            List<(char,int)> operations = Operation(input);
-            while (operations.Count > 0)
-            {
-                int operIndex = -1;
-                int max = 0;
-                for (int i = 0; i < operations.Count; i++)
+                if (Object.ReferenceEquals(expression[i].GetType(), check.GetType()))
                 {
-                    if (max < operations[i].Item2)
-                    {
-                        max = operations[i].Item2;
-                        operIndex = i;
-                    }
-                    if (max == operations[i].Item2)
-                    {
-                        if (operations[i].Item1 == '*' || operations[i].Item1 == '/')
-                        {
-                            operIndex = i;
-                        }
-                    }
-                }
-
-                if (operIndex == -1)
-                {
-                        operIndex = 0;
-                }
-                    
-                char op = operations[operIndex].Item1;
-                double num1 = numbers[operIndex];
-                double num2 = numbers[operIndex + 1];
-                double result = Calculate(op, num1, num2);
-                numbers.RemoveAt(operIndex + 1);
-                numbers.RemoveAt(operIndex);
-                operations.RemoveAt(operIndex);
-                numbers.Insert(operIndex, result);
-                
+                    double num1 = (double)expression[i - 2];
+                    double num2 = (double)expression[i - 1];
+                    double result = Calculate((char)expression[i], num1, num2);
+                    expression.RemoveAt(i);
+                    expression.RemoveAt(i - 1);
+                    expression[i - 2] = result;
+                    i -= 3;
+                } 
             }
-            Console.WriteLine(numbers[0]);
-            */
-
+            Console.WriteLine(expression[0]);
         }
     }
 }
