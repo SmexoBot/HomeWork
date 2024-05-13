0a1ca5133359591fc6db2c472bf6fb2e7e74b3dc
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-13 18:32:21 +0500

    new operation Devide

---
+++ Rpn.Logic/Devide.cs
@@ -0,0 +1,26 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Devide : Operation
+    {
+        public override string Name => "/";
+        public override int Priority => 2;
+        public override bool IsFunction => false;
+        public override int ArgsNumber => 2;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            double num2 = numbers[1].Number;
+            Numbers result = new Numbers(num1 / num2);
+            return result;
+        }
+
+    }
+}
