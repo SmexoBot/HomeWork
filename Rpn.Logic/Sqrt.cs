d91f9268fa1278981484d9372fde5a4a869c9c56
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-17 19:20:36 +0500

    new operation Sqrt

---
+++ Rpn.Logic/Sqrt.cs
@@ -0,0 +1,26 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Sqrt : Operation
+    {
+        public override string Name => "sqrt";
+        public override int Priority => 3;
+        public override bool IsFunction => true;
+        public override int ArgsNumber => 1;
+        public override double MinValue => 0;
+        public override double MaxValue => double.MaxValue;
+        public override double NotEqual => double.MinValue;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            return new Numbers(Math.Sqrt(num1));
+        }
+    }
+}
