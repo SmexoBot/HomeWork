b7940ef11bbdeee19da26bf03de0c7bbdaf9f547
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-17 19:20:04 +0500

    New Operation Ln

---
+++ Rpn.Logic/Ln.cs
@@ -0,0 +1,27 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Ln : Operation
+    {
+        public override string Name => "ln";
+        public override int Priority => 3;
+        public override bool IsFunction => true;
+        public override int ArgsNumber => 1;
+        public override double MinValue => 0;
+        public override double NotEqual => double.MinValue;
+        public override double MaxValue => double.MaxValue;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            return new Numbers(Math.Log(num1));
+        }
+
+    }
+}
