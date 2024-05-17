7b4b71e2a1061d14378255239b775fb73f30fac5
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-17 19:19:53 +0500

    new operation Ctg

---
+++ Rpn.Logic/Ctg.cs
@@ -0,0 +1,27 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Ctg : Operation
+    {
+        public override string Name => "ctg";
+        public override int Priority => 3;
+        public override bool IsFunction => true;
+        public override int ArgsNumber => 1;
+        public override double MinValue => double.MinValue;
+        public override double MaxValue => double.MaxValue;
+        public override double NotEqual => Math.PI;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            return new Numbers(Math.Cos(num1) / Math.Sin(num1));
+        }
+
+    }
+}
