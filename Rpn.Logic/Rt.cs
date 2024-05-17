c8e2e32084b3d1d1ca4c1f3fab2fc32ec347be38
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-17 19:20:25 +0500

    new operation rt

---
+++ Rpn.Logic/Rt.cs
@@ -0,0 +1,27 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Rt : Operation
+    {
+        public override string Name => "rt";
+        public override int Priority => 3;
+        public override bool IsFunction => true;
+        public override int ArgsNumber => 2;
+        public override double MinValue => double.MinValue;
+        public override double MaxValue => double.MaxValue;
+        public override double NotEqual => double.MinValue;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            double num2 = numbers[1].Number;
+            return new Numbers(Math.Pow(num2, 1 / num1));
+        }
+    }
+}
