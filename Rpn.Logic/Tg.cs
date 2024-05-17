2c7a9188c8cd9b36bba9e26a2d88e3bf25ee4a65
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-17 19:20:53 +0500

    new operation Tg

---
+++ Rpn.Logic/Tg.cs
@@ -0,0 +1,27 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Tg : Operation
+    {
+        public override string Name => "tg";
+        public override int Priority => 3;
+        public override bool IsFunction => true;
+        public override int ArgsNumber => 1;
+        public override double MinValue => double.MinValue;
+        public override double MaxValue => double.MaxValue;
+        public override double NotEqual => Math.PI / 2;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            return new Numbers(Math.Tan(num1));
+        }
+
+    }
+}
