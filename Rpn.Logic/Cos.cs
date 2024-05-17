087aa09dc6a2d297059d40f106322c8dc1f08356
Evgeniy Larushkin <larushkinevgeniy.ru@gmail.com>
2024-05-17 18:23:28 +0500

    new operation Cos

---
+++ Rpn.Logic/Cos.cs
@@ -0,0 +1,27 @@
+﻿using RpnLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpn.Logic
+{
+     class Cos : Operation
+    {
+        public override string Name => "cos";
+        public override int Priority => 3;
+        public override bool IsFunction => true;
+        public override int ArgsNumber => 1;
+        public override double MinValue => double.MinValue;
+        public override double MaxValue => double.MaxValue;
+        public override double NotEqual => double.MaxValue;
+
+        public override Numbers Execute(params Numbers[] numbers)
+        {
+            double num1 = numbers[0].Number;
+            return new Numbers(Math.Cos(num1));
+        }
+
+    }
+}
